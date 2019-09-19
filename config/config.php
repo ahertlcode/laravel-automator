c035b872f17043baab1c04506f9eec9df9389143
Abayomi Apetu <abayomismart@gmail.com>
2019-09-20 00:32:59 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
     return [
         "host" => "localhost",
         "port" => "3306",
-        "user" => "abayomi", 
-        "password" => "aherceo2", 
-        "database" => "fotoquest",
+        "user" => "test_user", 
+        "password" => "test12345", 
+        "database" => "test_db",
         "database_type" => "mysql",
     ];
