76e1bcef33b4763858f6b99cf8d9e1d7d6ba2d06
Azeez <temidoswag@gmail.com>
2022-09-13 12:27:58 +0100

    updated the controller to cater for foreign table inclusion

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@
         "port" => "3306",
         "user" => "root",
         "password" => "Tope1234$",
-        "database" => "school_managers",
+        "database" => "school_manager",
         "database_type" => "mysql",
         "excludeTables" => ['migrations'],
         "excludeColumns" => ['id','status','password_digest','password_reset_token','api_token','remember_token','createdAt','created_at','updatedAt','updated_at','deletedAt','deleted_at','user_id','owner_id']
